f029c70a887cc1f6579ab3658533ef348d208190
Gonza0086 <hgonzalo2000@gmail.com>
2025-04-29 14:20:44 -0300

    transparent bg

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,5 +2,7 @@ return {
     "rebelot/kanagawa.nvim",
     config = function()
         vim.cmd([[colorscheme kanagawa-dragon]])
+        vim.api.nvim_set_hl(0, "normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "normalFloat", { bg = "none" })
     end
 }
