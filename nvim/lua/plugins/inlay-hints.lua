517ffb825a19ba9274ead705b1b7689182555130
Gonza0086 <hgonzalo2000@gmail.com>
2025-06-11 14:38:02 -0300

    update

--- nvim/lua/plugins/inlay-hints.lua
+++ nvim/lua/plugins/inlay-hints.lua
@@ -4,6 +4,6 @@ return {
     dependencies = { "neovim/nvim-lspconfig" },
     config = function()
         require("inlay-hints").setup()
-        vim.api.nvim_set_hl(0, "LspInlayHint", { bg = "background", fg = "#4d627d" })
+        vim.api.nvim_set_hl(0, "LspInlayHint", { bg = "#181616", fg = "#4d627d" })
     end
 }
