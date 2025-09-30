f3cd15fe2d12d7ca90814809581274db7887a6e4
gonza0086 <hgonzalo2000@gmail.com>
2025-09-29 22:30:14 -0300

    git user config

--- install_fedora.sh
+++ install_fedora.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-# distro
+# git
+git config --global user.email "hgonzalo2000@gmail.com"
+git config --global user.name "gonza0086"
+
+# distro & essentials
 sudo dnf update --refresh # update fedora
 sudo dnf install -y ncurses ripgrep tmux neovim
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # rust
+sudo dnf install -y postgresql-server postgresql-contrib # postgresql
+sudo systemctl enable postgresql
 
 # bashrc
 mv ~/.bashrc ~/.bashrc.ori
-ln -s ~/dotfiles/bashrc .bashrc
+ln -s ~/dotfiles/bashrc  ~/.bashrc
 
 # tmux
 ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
@@ -14,5 +21,5 @@ mkdir ~/.tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # nvim
-mkdir .config
+mkdir ~/.config
 ln -s ~/dotfiles/nvim ~/.config/nvim
